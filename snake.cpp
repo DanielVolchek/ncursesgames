b6c8b2d595e375d7eaebf34c8f4a660af805b26c
danielvolchek <danielvolchek123@gmail.com>
2021-09-27 13:35:26 -0400

    test of input

--- snake.cpp
+++ snake.cpp
@@ -6,11 +6,11 @@
 #include <ncurses.h> // g++ -l ncurses
 using namespace std;
 
-#define UP_KEY 72
-#define LEFT_KEY 75
-#define RIGHT_KEY 77
-#define DOWN_KEY 80
-
+#define UP_KEY 38
+#define LEFT_KEY 37
+#define RIGHT_KEY 39
+#define DOWN_KEY 40
+#define ENTER_KEY 13
 void initScreen();
 void logic();
 void drawScreen();
@@ -19,13 +19,26 @@ void getInput();
 
 int main(){
     initScreen();
-   
+    getInput();   
     return 0;
 }
 
 void initScreen(){
     initscr();
+    noecho();
+    raw();
 
-
+}
+void getInput(){
+    char c = _getch();
+    bool running = true;
+    while (running){
+        switch (c){
+            case 'w': printw("west"); break;
+            case UP_KEY: printw("west"); break;
+            case 'q': running = false; break;
+        }
+    }
+    endwin();
 
 }

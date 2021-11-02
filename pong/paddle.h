703670365d986608b226087e76b97ff3d6f9c714
danielvolchek <danielvolchek123@gmail.com>
2021-11-01 20:49:35 -0400

    first iteration

---
+++ pong/paddle.h
@@ -0,0 +1,19 @@
+//File name:/home/danielvolchek/ncursesgames/pong/paddle.h
+//Daniel Volchek
+//Section_01
+
+//Code:
+#include <iostream>
+using namespace std;
+class paddle{
+    public:
+        int x;
+        int y;
+        int length;
+        int width = 3;
+        int score = 0;
+        void setCoord(int moveX, int moveY){
+            x = moveX;
+            y = moveY;
+        }
+};
